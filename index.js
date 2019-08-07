5c852b18d81f1ca186f2a24d4debc8240feba631
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-07 18:32:19 -0400

    first get request working in postman, get request to return array of users working also

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // implement your API here
+
+// importing libraries
+const express = require('express');
+
+// import other files needed
+const db = require('./data/db');
+
+// import global objects
+// create simple server
+const server = express();
+
+// adding middleware
+server.use(express.json());
+
+// GET request to test if code is working properly
+server.get('/', (req, res) => {
+    res.send('Hello From Cobrettie')
+});
+
+// GET request to return an array of all user objects contained in the database
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users)
+            console.log('users', users)
+        })
+        .catch(err => {
+            res.status(418).json({
+                err: err,
+                message: 'error'
+            })
+        })
+});
+
+// // POST method
+// server.post('/api/users', (req, res) => {
+//     const newUser = req.body;
+//     console.log('newUser', newUser);
+// })
+
+// server now ready to receive requests
+server.listen(4000, () => {
+    console.log('server is running on port 4000...');
+});
